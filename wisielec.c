b7000c996915caf2679abf6ce9d54ae3c5f507e4
pawelwuuu <wojcikp376@gmail.com>
2023-01-05 23:26:15 +0100

    added bunch of functions which made game playable

--- wisielec.c
+++ wisielec.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "drawing.h"
 #include <string.h>
 #include <stdbool.h>
 #define maxWordLen 20
@@ -10,15 +11,23 @@ typedef struct
 	char nickname[21];
 	char gameResult[10];
 	char guessWord[maxWordLen];
+	char usedChars[60];
+	bool isOn;
 	int attempts;
+	int badAnswers;
 	int guessesInRow;
-}
+	char userProgress[maxWordLen];
+}TGame;
 
-TGame;
 
 void readLine(const char *, int, char *);
 int getRandomNum();
+void gameplay(TGame*);
+void checkGuess(TGame*, char);
 TGame initGame(const char *);
+bool isUsed(TGame*, char c);
+void printRevealed(TGame*);
+void updateGameSt(TGame*);
 
 int main(int argc, char **argv)
 {
@@ -32,6 +41,7 @@ int main(int argc, char **argv)
 	}
 
 	TGame game = initGame(guessWord);
+	gameplay(&game);
 
 	// printf("nick:%s zgadywanie:%s proby:%d jak:%s ciag:%d to dane", game.nickname, game.guessWord, game.attempts, game.gameResult, game.guessesInRow);
 
@@ -81,8 +91,88 @@ TGame initGame(const char *guessWord)
 	strcpy(game.nickname, nickname);
 	strcpy(game.gameResult, "Undefined");
 	strcpy(game.guessWord, guessWord);
+	game.isOn = true;
 	game.attempts = 0;
 	game.guessesInRow = 0;
-
+	game.badAnswers = 0;
+	for (int i = 0; guessWord[i] != 0; i++)
+	{
+		game.userProgress[i] = '_';
+	}
+	
 	return game;
+}
+
+void checkGuess(TGame *game, char userGuess){
+	bool isPresent = false;
+	for (int i = 0; game -> guessWord[i] != 0; i++){
+		// char a = game -> guessWord[i];
+		// char c = game -> userProgress[i];
+
+		if ((game -> guessWord[i] == userGuess) && (game -> userProgress[i] == '_')){
+			game -> userProgress[i] = userGuess;
+			isPresent = true;
+		}
+	}
+	if (! isPresent)
+		game -> badAnswers++;
+}
+
+bool isUsed(TGame *game, char c){
+	for (int i=0; i < sizeof(game -> usedChars); i++){
+		if (game -> usedChars[i] == c)
+			return true;
+	}
+
+	for (int i=0; i < sizeof(game -> usedChars); i++){
+		if (game -> usedChars[i] == 0){
+			game -> usedChars[i] = c;
+			return false;
+		}
+	}
+}
+
+void printRevealed(TGame *game){
+	printf("\nSTATUS: ");
+	for (int i = 0; game -> userProgress[i]; i++){
+		printf("%c", game -> userProgress[i]);
+	}
+	printf("\n");
+}
+
+void updateGameSt(TGame *game){
+	if (strcmp(game -> userProgress, game -> guessWord) == 0){
+		game -> isOn = false;
+		strcpy(game -> gameResult, "WIN");
+		printf("WYGRALES! :D");
+	} else if (game -> badAnswers > 10){
+		game -> isOn = false;
+		strcpy(game -> gameResult, "LOSE");
+		printf("PRZEGRALES :(");
+	}
+}
+
+void gameplay(TGame *game){
+	printf("Twoje slowo ma %d liter, sprobuj je odgadnac:\n", strlen(game -> guessWord));
+	while(game -> isOn){
+		char userGuess;
+		printf("Podaj znak:\n");
+		fflush(stdin);
+		scanf("%c", &userGuess);
+		if (isUsed(game, userGuess)){
+			printf("Uzyles juz znaku '%c'.\n", userGuess);
+			continue;
+		}
+
+		checkGuess(game, userGuess);
+
+		if (game -> badAnswers > 0)
+			draw(game -> badAnswers);
+		
+		printRevealed(game);
+
+		updateGameSt(game);
+
+		
+	}
 }
